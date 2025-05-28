eff5174c86fc52732c9a4f644d3673241d096537
wpplng <lina.wappling@gmail.com>
2025-05-28 14:47:34 +0200

    Option one show ticket price

--- FlowControl/Program.cs
+++ FlowControl/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection;
 using System.Runtime.ConstrainedExecution;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FlowControl
 {
@@ -18,9 +22,10 @@ namespace FlowControl
                 Console.WriteLine("\n=== HUVUDMENY ===");
                 Console.WriteLine("0: Avsluta programmet");
                 // 4.Bygg ut menyn med val att exekvera de övriga övningarna.
-                Console.WriteLine("1: Ungdom eller pensionär");
-                Console.WriteLine("2: Upprepa tio gånger");
-                Console.WriteLine("3: Det tredje ordet");
+                Console.WriteLine("1: Biljettpris - Ungdom eller pensionär");
+                Console.WriteLine("2: Räkna ut totalpris");
+                Console.WriteLine("3: Upprepa tio gånger");
+                Console.WriteLine("4: Det tredje ordet");
             switch (Console.ReadLine())
             {
                 case "0":
@@ -28,12 +33,20 @@ namespace FlowControl
                     isRunning = false;
                     break;
                 case "1":
-                    Console.WriteLine("Ungdom eller pensionär.");
+                    // Menyval 1: Ungdom eller pensionär
+                    Console.WriteLine("Biljettpris - Ungdom eller pensionär");
+                    ShowTicketPrice();
                     break;
                 case "2":
-                    Console.WriteLine("Upprepa tio gånger.");
+                    Console.WriteLine("Räkna ut totalpris");
+                    // Vi vill även få möjlighet att kunna räkna ut priset för ett helt sällskap.Lägg till det alternativet i huvudmenyn(ett case “2”). Det är även ok att ha alternativet i en undermeny. Vi anger då först hur många vi är som ska gå på bio.Frågar sedan efter ålder på var och en och skriver sedan ut en sammanfattning i konsolen som ska innehålla följande:
+                    // ● Antal personer
+                    // ● Samt totalkostnad för hela sällskapet
                     break;
                 case "3":
+                    Console.WriteLine("Upprepa tio gånger.");
+                    break;
+                case "4":
                     Console.WriteLine("Det tredje ordet.");
                     break;
                 default:
@@ -42,6 +55,40 @@ namespace FlowControl
             }
                 
             }
+
+        }
+            private static void ShowTicketPrice()
+            {
+                // För att exemplifiera if-satser skall ni implementera, på uppdrag av en teoretisk lokal bio, ett program som kollar om en person är pensionär eller ungdom vid angiven ålder. För attkomma till denna funktion skall ett case i huvudmenyn skapas för ”1”, detta skall även framgå i texten som förklarar menyn. För att göra detta skall ni använda er av en nästlad if-sats.Det skall ske enligt följande förlopp:
+                Console.WriteLine("Var vänlig och ange din ålder:");
+                // 1.Användaren anger en ålder i siffror 
+                string ageInput = Console.ReadLine()!;
+                int age;
+                // 2.Programmet konverterar detta från en sträng till en int
+                if (int.TryParse(ageInput, out age))
+                {
+                    // 3.Programmet ser om personen är ungdom(under 20 år)
+                    if (age < 20)
+                    {
+                        // 4.Om det ovanstående är sant skall programmet skriva ut: Ungdomspris: 80kr
+                        Console.WriteLine("Ungdomspris: 80kr");
+                    }
+                    // 5.Annars kollar programmet om personen är en pensionär(över 64 år)
+                    else if (age > 64)
+                    {
+                        // 6.Om ovanstående är sant skall programmet skruva ut: Pensionärspris: 90kr
+                        Console.WriteLine("Pensionärspris: 90kr");
+                    }
+                    else
+                    {
+                        // 7.Annars skall programmet skriva ut: Standardpris: 120kr
+                        Console.WriteLine("Standardpris: 120kr");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Felaktig ålder, var vänlig försök igen.");
+                }
         }
     }
 }
